82e4f58ef57e7e9549f481be62c3d625e104c9cc
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-03-04 15:04:21 +0200

    Focusable icons

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -8,11 +8,17 @@ interface IconProps {
 
 const Icon: React.FC<IconProps> = ({ name, icon, classOfComponent }) => {
   const [display, setDisplay] = useState(false);
+  const [timesClicked, setTimesClicked] = useState(0);
+
+  const handleClick = () => {
+    setDisplay(!display);
+    setTimesClicked(timesClicked === 2 ? 0 : timesClicked + 1);
+  };
 
   return (
-    <div className="shortcut clickable">
+    <div className="shortcut clickable" tabIndex={0} onClick={handleClick}>
       <img src={icon} className="icon" />
-      <span className="name">{name}</span>
+      <span className={"name"}>{name}</span>
     </div>
   );
 };
