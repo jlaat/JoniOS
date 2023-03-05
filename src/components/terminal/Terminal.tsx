9662e186fce024473d6c25c1daca54f7d894a13c
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-03-05 19:30:07 +0200

    Created some commands for terminal

--- src/components/terminal/Terminal.tsx
+++ src/components/terminal/Terminal.tsx
@@ -1,17 +1,48 @@
 import { useState } from "react";
+import { commands } from "./commands";
 
 const Terminal = () => {
+  const [input, setInput] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleEnterPressed = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      switch (input) {
+        case "help": {
+          console.log(commands[0]);
+          break;
+        }
+        case "who": {
+          console.log(commands[1]);
+          break;
+        }
+        case "opening_banner": {
+          console.log(commands[3]);
+          break;
+        }
+      }
+    }
+  };
+
   return (
-    <div className="terminal">
+    <div className="terminal" onKeyDown={handleEnterPressed}>
       <div className="terminal-header">
         <span>opaa</span>
         <span>you@joniOS</span>
         <button className="terminal-close">x</button>
       </div>
-      <div className="terminal-body">as</div>
+      <div className="terminal-body"></div>
       <div className="terminal-input">
         <span className="terminal-input-prompt">you@joniOS:~$</span>
-        <input className="terminal-input-content" />
+        <input
+          className="terminal-input-content"
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+        />
       </div>
     </div>
   );
