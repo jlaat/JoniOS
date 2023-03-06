2376d26ae979eb7674124b35ffb316c25ea729a3
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-03-06 20:19:29 +0200

    Terminal ready, need to check if css can be cleaned

--- src/components/terminal/Terminal.tsx
+++ src/components/terminal/Terminal.tsx
@@ -1,40 +1,109 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { commands } from "./commands";
+import Ruksi from "../.././assets/icons/ruksi.png";
+
+interface ExecutedCommand {
+  command: string;
+  output: string[];
+}
 
 const Terminal = () => {
   const [input, setInput] = useState("");
+  const [history, setHistory] = useState<ExecutedCommand[]>([]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setInput(e.target.value);
   };
 
-  const handleEnterPressed = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleEnterPressed = (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ): void => {
     if (e.key === "Enter") {
       switch (input) {
         case "help": {
-          console.log(commands[0]);
+          addCommandToHistory({ command: input, output: commands[0] });
           break;
         }
         case "who": {
           console.log(commands[1]);
+          addCommandToHistory({ command: input, output: commands[1] });
           break;
         }
-        case "opening_banner": {
-          console.log(commands[3]);
+        case "skills": {
+          addCommandToHistory({ command: input, output: commands[2] });
+          break;
+        }
+        case "clear": {
+          setHistory([]);
+          setInput("");
           break;
         }
       }
     }
   };
 
+  const addCommandToHistory = (command: ExecutedCommand) => {
+    setHistory([...history, command]);
+    setInput("");
+    scrollTerminalToBottom();
+  };
+
+  // Scroll terminal COMPLETELY to bottom
+  const scrollTerminalToBottom = () => {
+    const terminal = document.querySelector(".terminal");
+    if (terminal) {
+      terminal.scrollTop = terminal.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollTerminalToBottom();
+  }, [history]);
+
+  // User can start writing commands after clicking terminal
+  const giveInputFocus = () => {
+    const input = document.querySelector(
+      ".terminal-input-content"
+    ) as HTMLInputElement;
+    if (input) {
+      input.focus();
+    }
+  };
+
   return (
-    <div className="terminal" onKeyDown={handleEnterPressed}>
+    <div
+      className="terminal"
+      onKeyDown={handleEnterPressed}
+      onClick={giveInputFocus}
+    >
       <div className="terminal-header">
         <span>opaa</span>
         <span>you@joniOS</span>
-        <button className="terminal-close">x</button>
+        <img src={Ruksi} className="terminal-close" />
       </div>
-      <div className="terminal-body"></div>
+
+      {history.map((command, index) => (
+        <div className="command-container" key={index}>
+          <span className="terminal-input-prompt">
+            <br />
+            you@joniOS:~${" "}
+          </span>
+          <span>{command.command}</span>
+          <br />
+          <div className="command-container">
+            {command.output.map((line) => {
+              return (
+                <span>
+                  <br />
+                  {line}
+                </span>
+              );
+            })}
+          </div>
+          <br />
+        </div>
+      ))}
+
       <div className="terminal-input">
         <span className="terminal-input-prompt">you@joniOS:~$</span>
         <input
--- src/components/terminal/commands.ts
+++ src/components/terminal/commands.ts
@@ -1,43 +1,22 @@
 const who: string[] = [
-  "<br>",
   "Hi, I'm Joni.",
   "I'm an IT student, career changer and a future software developer.",
   "Changed career from a roofer to a software developer to put my passion for technology to good use. Best decision I've made.",
-  "What interests me about software development is the creative aspect of it combined with logical thinking.",
+  "I find software development captivating due to the need for logical reasoning and the ability to craft something out of nothing by following a set of rules.",
   "I'm currently studying at the Karelia University of Applied Sciences.",
 ];
 
 const skills: string[] = [
-  "<br>",
-  "I see myself as great systematic thinker with good working life skills.",
+  "I see myself as a great systematic thinker with good working life skills.",
   "What comes to my programming skills, I'm constantly trying and learning skills that I find interesting and fun. It is important for me that coding is fun.",
   "Since 09/2021 I've been learning different aspects of software development very widely.",
   "I've been learning various programming languages, frameworks, databases, agile methods, list goes on. (and more to come!)",
-  "<br>",
 ];
 
 const help: string[] = [
-  "<br>",
-  '<span class="command">who</span> - About Joni',
-  '<span class="command">skills</span> - My skills',
-  '<span class="command">clear</span> - Clear the terminal',
-  "<br>",
+  "who" + "\xa0\xa0\xa0" + " About Joni",
+  "skills" + "\xa0\xa0" + " My skills",
+  "clear" + "\xa0\xa0" + " Clear the terminal",
 ];
 
-const opening_banner: string[] = [
-  "<br>",
-  "        _____          ",
-  "    ,-:` ;',`'-,      ",
-  "  .'-;_,;  ':-;_,'.    ",
-  " /;   '/    ,  _`.-          _ _                 _   ",
-  "| '`. (`     /` ` `|        | | |               | |  ",
-  "|:.  ``-.   _   / |         | | |     __ _  __ _| |_ ",
-  "|     (   `,  .` ;'|    _   | | |    / _` |/ _` | __|",
-  "      | .'     `-'/    | |__| | |___| (_| | (_| | |_ ",
-  "  `.   ;/        .'       ____/|________,_|__,_|__|",
-  "    `'-._____.         ",
-
-  "<br>",
-];
-
-export const commands = [help, who, skills, opening_banner];
+export const commands = [help, who, skills];
